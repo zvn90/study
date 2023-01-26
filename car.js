21a07d2743f853a928fa88a31996b97c886f717b
zvn90 <115043149+zvn90@users.noreply.github.com>
2023-01-26 12:43:03 +0300

    2. Синтаксис

---
+++ car.js
@@ -0,0 +1,21 @@
+const carName = 'BMW'
+let maxCarSpeed = '250'
+let carOwner = 'Вадим'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = '240'
+console.log('У машины изменилась скорость', maxCarSpeed)
+maxCarSpeed = '235'
+console.log('У машины изменилась скорость', maxCarSpeed)
+maxCarSpeed = '230'
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+carOwner = 'Алексей'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Андрей'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Олег'
+console.log('У машины изменился владелец', carOwner)
---
+++ index.js
@@ -0,0 +1,15 @@
+//'JavaScript-код с созданием переменных'
+/*'Автор:'
+'zvn90'*/
+
+const myName = 'Вадим'
+const mySurname = 'Золкин'
+let myFavoriteDrink = 'Кофе'
+let myFavoriteAnimal = 'Кошка'
+let myFavoriteProgrammingLanguage = 'JavaScript'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
